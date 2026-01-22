20874dc1a027a4939f7948cd949d91a2fb408090
Rishav Jindal <rishavj0212@gmail.com>
2026-01-22 23:11:29 +0530

    correct timezone

--- src/main/java/com/example/flowable_app/FlowableAppApplication.java
+++ src/main/java/com/example/flowable_app/FlowableAppApplication.java
@@ -5,30 +5,41 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
 // 👇 1. Define the API Information
 @OpenAPIDefinition(
-		info = @Info(title = "Flowable App Backend", version = "1.0", description = "Combined API for Custom App & Flowable Engine"),
-		security = @SecurityRequirement(name = "bearerAuth") // Applies security globally
+        info = @Info(title = "Flowable App Backend",
+                version = "1.0",
+                description = "Combined API for Custom App & Flowable Engine"),
+        security = @SecurityRequirement(name = "bearerAuth") // Applies security globally
 )
 // 👇 2. Define the "Authorize" Button (JWT)
 @SecurityScheme(
-		name = "bearerAuth",
-		type = SecuritySchemeType.HTTP,
-		scheme = "bearer",
-		bearerFormat = "JWT"
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class FlowableAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowableAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableAppApplication.class, args);
+    }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 }
+
+
